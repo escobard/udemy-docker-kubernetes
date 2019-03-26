aac222a70fad4dabf327cc16677aa02f2327dc58
Daniel Escobar <escobard90@gmail.com>
2019-03-26 14:44:28 -0600

    section 4 - feat: added base docker file, intentional npm error

--- section4/simpleweb/index.js
+++ section4/simpleweb/index.js
@@ -1,7 +1,12 @@
-const express = require('express');
+const express = require('express'),
+port = 8080;
 
 const app = express();
 
 app.get('/', (req, res) =>{
     res.send('Hi there');
-})
+});
+
+app.listen(port, () =>{
+    console.log(`Listening on ${port}`)
+});
